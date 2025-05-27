ece0363cf7dc33b73d0645da2bc15fd04b733cd9
pcaldito <147615008+pcaldito@users.noreply.github.com>
2025-05-27 20:41:04 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -2,5 +2,5 @@ CREATE TABLE usuarios(
     idUsuario smallint unsigned AUTO_INCREMENT PRIMARY KEY,
     nombre varchar(150) NOT NULL,
     correo varchar(90) NOT NULL UNIQUE,
-    pw char(16) NOT NULL
+    pw varchar(255) NOT NULL
 );
